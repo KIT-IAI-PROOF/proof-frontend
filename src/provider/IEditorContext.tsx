0bdb0b8f9064efb9ff0806134ee726f06832c5c8
Michael Maier <michael.maier@kit.edu>
2026-08-06 16:47:08 +0200

    fix: remove files that were wrongly added in merge

--- src/provider/IEditorContext.tsx
+++
@@ -1,4 +0,0 @@
-import {Context, createContext} from "react";
-import {IEditorContext} from "./EditorProvider.tsx";
-
-export const EditorContext: Context<IEditorContext> = createContext({} as IEditorContext);
--- src/provider/IMonitoringContext.tsx
+++
@@ -1,4 +0,0 @@
-import {Context, createContext} from "react";
-import {IMonitoringContext} from "./MonitoringProvider.tsx";
-
-export const MonitoringContext: Context<IMonitoringContext> = createContext({} as IMonitoringContext);
